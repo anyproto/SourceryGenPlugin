ed24fc9e7512ef3e2aa7642a6e4c1f5328b8603d
Mikhail Golovko <m@anytype.io>
2023-01-13 12:56:54 +0300

    Release 1.9.2

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+  name: "SourceryGenPlugin",
+  products: [
+    .plugin(name: "SourceryGenPlugin", targets: ["SourceryGenPlugin"])
+  ],
+  dependencies: [],
+  targets: [
+    .plugin(
+      name: "SourceryGenPlugin",
+      capability: .buildTool(),
+      dependencies: ["sourcery"]
+    ),
+    .binaryTarget(
+      name: "sourcery",
+      url: "https://github.com/mgolovko/SourceryGenPlugin/releases/download/1.9.2/sourcery-1.9.2.artifactbundle.zip",
+      checksum: "f176e608354386fc8622f343ffd4ebed09cf8f0ba175052904f86c2c988c6fab"
+    )
+  ]
+)
---
+++ Plugins/SourceryGenPlugin/Plugin.swift
@@ -0,0 +1,65 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct SourceryGenPlugin: BuildToolPlugin {
+  func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+    let fileManager = FileManager.default
+
+    let configurations: [Path] = [context.package.directory, target.directory]
+      .map { $0.appending("sourcery.yml") }
+      .filter { fileManager.fileExists(atPath: $0.string) }
+
+    guard validate(configurations: configurations, target: target) else {
+      return []
+    }
+
+    fileManager.forceClean(directory: context.pluginWorkDirectory)
+
+    return try configurations.map { configuration in
+      try .swiftgen(using: configuration, context: context, target: target)
+    }
+  }
+}
+
+// MARK: - Helpers
+
+private extension SourceryGenPlugin {
+  /// Validate the given list of configurations
+  func validate(configurations: [Path], target: Target) -> Bool {
+    guard !configurations.isEmpty else {
+      Diagnostics.error("""
+      No SourceryGen configurations found for target \(target.name). If you would like to generate sources for this \
+      target include a `sourcery.yml` in the target's source directory, or include a shared `sourcery.yml` at the \
+      package's root.
+      """)
+      return false
+    }
+
+    return true
+  }
+}
+
+private extension Command {
+  static func swiftgen(using configuration: Path, context: PluginContext, target: Target) throws -> Command {
+    .prebuildCommand(
+      displayName: "SourceryGen BuildTool Plugin",
+      executable: try context.tool(named: "sourcery").path,
+      arguments: [
+        "--config",
+        configuration,
+        "--cacheBasePath",
+        context.pluginWorkDirectory.string
+      ],
+      outputFilesDirectory: context.pluginWorkDirectory
+    )
+  }
+}
+
+private extension FileManager {
+  /// Re-create the given directory
+  func forceClean(directory: Path) {
+    try? removeItem(atPath: directory.string)
+    try? createDirectory(atPath: directory.string, withIntermediateDirectories: false)
+  }
+}
