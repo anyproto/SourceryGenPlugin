97163be62fb7bb21bc150f4e6d26269dd73b9534
Mikhail Golovko <m@anytype.io>
2023-06-06 11:29:30 +0300

    Fix url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ),
     .binaryTarget(
       name: "sourcery",
-      url: "https://github.com/mgolovko/SourceryGenPlugin/releases/download/1.9.2/sourcery-1.9.2.artifactbundle.zip",
+      url: "https://github.com/anyproto/SourceryGenPlugin/releases/download/1.9.2/sourcery-1.9.2.artifactbundle.zip",
       checksum: "f176e608354386fc8622f343ffd4ebed09cf8f0ba175052904f86c2c988c6fab"
     )
   ]
